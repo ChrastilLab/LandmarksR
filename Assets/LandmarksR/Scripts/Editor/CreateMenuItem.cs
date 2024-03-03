7a971aa6b5a027946ea786ca71172e0819b70595
GreasyCat <131315c@gmail.com>
2024-03-02 19:39:15 -0800

    Implement Context Menu for Quick Task Addition
    - New Feature: Introduce context menu to quickly add Tasks.
    - Task Management: Include multiple tasks, ordered by purpose.

---
+++ Assets/LandmarksR/Scripts/Editor/CreateMenuItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace LandmarksR.Scripts.Editor
+{
+    public static class CreateMenuItem
+    {
+        public static void CreateGameObjectContextMenu<T>(MenuCommand menuCommand, string name) where T: Component
+        {
+            var go = new GameObject(name);
+            go.AddComponent<T>();
+            GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+            Selection.activeObject = go;
+        }
+    }
+}
---
+++ Assets/LandmarksR/Scripts/Experiment/Tasks/Editor/HierarchyMenu.cs
@@ -0,0 +1,41 @@
+﻿using LandmarksR.Scripts.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace LandmarksR.Scripts.Experiment.Tasks.Editor
+{
+    public class HierarchyMenu: MonoBehaviour
+    {
+        [MenuItem("GameObject/Experiment/1. RootTask", false, 1)]
+        private static void CreateRootTask(MenuCommand menuCommand)
+        {
+            CreateMenuItem.CreateGameObjectContextMenu<RootTask>(menuCommand, "RootTask");
+        }
+
+        [MenuItem("GameObject/Experiment/2. InstructionTask", false, 1)]
+        private static void CreateInstructionTask(MenuCommand menuCommand)
+        {
+            CreateMenuItem.CreateGameObjectContextMenu<InstructionTask>(menuCommand, "InstructionTask");
+        }
+
+        [MenuItem("GameObject/Experiment/3. DummyTask", false, 1)]
+        private static void CreateDummyTask(MenuCommand menuCommand)
+        {
+            CreateMenuItem.CreateGameObjectContextMenu<DummyTask>(menuCommand, "DummyTask");
+        }
+
+        [MenuItem("GameObject/Experiment/4. RepeatTask", false, 1)]
+        private static void CreateRepeatTask(MenuCommand menuCommand)
+        {
+            CreateMenuItem.CreateGameObjectContextMenu<RepeatTask>(menuCommand, "RepeatTask");
+        }
+
+        [MenuItem("GameObject/Experiment/5. ExploreTask", false, 1)]
+        private static void CreateExploreTask(MenuCommand menuCommand)
+        {
+            CreateMenuItem.CreateGameObjectContextMenu<ExploreTask>(menuCommand, "ExploreTask");
+        }
+
+
+    }
+}
