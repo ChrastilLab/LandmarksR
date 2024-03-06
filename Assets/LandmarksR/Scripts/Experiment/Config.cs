ea43772b4bcf69e84d5028901a83f04f7f60827c
GreasyCat <131315c@gmail.com>
2024-03-06 09:25:32 -0800

    Implement Config Class for Environmental Variables Management
    - Add Config class to centralize the management of environmental variables.

---
+++ Assets/LandmarksR/Scripts/Experiment/Config.cs
@@ -0,0 +1,51 @@
+﻿using LandmarksR.Scripts.Player;
+using UnityEngine;
+
+namespace LandmarksR.Scripts.Experiment
+{
+
+
+
+    public class Config : MonoBehaviour
+    {
+        public static Config Instance => _instance ??= BuildConfig();
+        private static Config _instance;
+
+        private static Config BuildConfig()
+        {
+            return new GameObject("Config").AddComponent<Config>();
+        }
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+
+        [SerializeField] private DisplayMode displayMode = DisplayMode.Desktop;
+        [SerializeField] private HudMode hudMode = HudMode.Follow;
+
+        public DisplayMode DisplayMode {get => displayMode; private set => displayMode = value;}
+        public HudMode HudMode {get => hudMode; private set => hudMode = value;}
+
+        public void UpdateDisplayMode(DisplayMode mode, PlayerController playerController)
+        {
+            DisplayMode = mode;
+            playerController.SwitchDisplayMode(mode);
+        }
+
+        public void UpdateHudMode(HudMode mode, Hud hud)
+        {
+            HudMode = mode;
+            hud.SwitchHudMode(mode);
+        }
+
+
+    }
+}
