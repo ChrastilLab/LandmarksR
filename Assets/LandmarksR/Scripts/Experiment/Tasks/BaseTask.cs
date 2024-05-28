214d434e452639b81e3f5d353eb14bf94f81da77
greasycat <131315c@gmail.com>
2024-05-28 07:06:40 -0700

    Add XML tag comments to BaseTask

--- Assets/LandmarksR/Scripts/Experiment/Tasks/BaseTask.cs
+++ Assets/LandmarksR/Scripts/Experiment/Tasks/BaseTask.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -10,34 +9,100 @@ using UnityEngine.Assertions;
 
 namespace LandmarksR.Scripts.Experiment.Tasks
 {
+    /// <summary>
+    /// Base class for all experiment tasks.
+    /// Handles common task functionality such as timers and subtask management.
+    /// </summary>
     public class BaseTask : MonoBehaviour
     {
-
+        /// <summary>
+        /// Indicates whether the task is enabled.
+        /// </summary>
         protected bool _enable;
-        [NotEditable] public uint id;
 
+        /// <summary>
+        /// Unique identifier for the task.
+        /// </summary>
+        [NotEditable]
+        public uint id;
+
+        /// <summary>
+        /// Gets the settings for the task.
+        /// </summary>
         protected Settings Settings { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the experiment instance associated with the task.
+        /// </summary>
         private Experiment Experiment { get; set; }
+
+        /// <summary>
+        /// Gets the player controller for the task.
+        /// </summary>
         protected PlayerController Player { get; private set; }
+
+        /// <summary>
+        /// Gets the player event controller for the task.
+        /// </summary>
         protected PlayerEventController PlayerEvent { get; private set; }
+
+        /// <summary>
+        /// Gets the HUD (Heads-Up Display) for the task.
+        /// </summary>
         protected Hud HUD { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the experiment logger for the task.
+        /// </summary>
         protected ExperimentLogger Logger { get; set; }
 
-        // Subtasks (if any) are directly loaded from the children of the task
+        /// <summary>
+        /// List of subtasks for this task.
+        /// </summary>
         protected List<BaseTask> _subTasks = new();
 
+        /// <summary>
+        /// Timer settings for the task.
+        /// </summary>
         [Header("Time")]
-        [SerializeField] protected float timer = Mathf.Infinity;
-        [SerializeField] protected bool randomizeTimer;
-        [SerializeField] private float minTimer = 0;
-        [SerializeField] private float maxTimer = 10;
-        [NotEditable, SerializeField] protected float elapsedTime;
+        [SerializeField]
+        protected float timer = Mathf.Infinity;
 
+        /// <summary>
+        /// Indicates whether the timer should be randomized.
+        /// </summary>
+        [SerializeField]
+        protected bool randomizeTimer;
+
+        /// <summary>
+        /// Minimum value for the timer when randomized.
+        /// </summary>
+        [SerializeField]
+        private float minTimer = 0;
+
+        /// <summary>
+        /// Maximum value for the timer when randomized.
+        /// </summary>
+        [SerializeField]
+        private float maxTimer = 10;
+
+        /// <summary>
+        /// Elapsed time since the task started.
+        /// </summary>
+        [NotEditable, SerializeField]
+        protected float elapsedTime;
+
+        /// <summary>
+        /// Unity Awake method. Initializes the task.
+        /// </summary>
         private void Awake()
         {
             _enable = true;
         }
 
+        /// <summary>
+        /// Unity Start method. Initializes the list of subtasks.
+        /// </summary>
         protected virtual void Start()
         {
             _subTasks = transform.Cast<Transform>()
@@ -47,11 +112,32 @@ namespace LandmarksR.Scripts.Experiment.Tasks
                 .ToList();
         }
 
-        [NotEditable, SerializeField] protected bool isRunning;
-        [NotEditable, SerializeField] protected bool isSubTaskRunning;
-        [NotEditable, SerializeField] protected bool isCompleted;
+        /// <summary>
+        /// Indicates whether the task is currently running.
+        /// </summary>
+        [NotEditable, SerializeField]
+        protected bool isRunning;
+
+        /// <summary>
+        /// Indicates whether a subtask is currently running.
+        /// </summary>
+        [NotEditable, SerializeField]
+        protected bool isSubTaskRunning;
+
+        /// <summary>
+        /// Indicates whether the task is completed.
+        /// </summary>
+        [NotEditable, SerializeField]
+        protected bool isCompleted;
+
+        /// <summary>
+        /// Indicates whether the task has been prepared.
+        /// </summary>
         private bool isPrepared;
 
+        /// <summary>
+        /// Prepares the task for execution.
+        /// </summary>
         protected virtual void Prepare()
         {
             Settings = Settings.Instance;
@@ -69,7 +155,6 @@ namespace LandmarksR.Scripts.Experiment.Tasks
             Assert.IsNotNull(Logger, $"{name} is missing a reference to Logger");
             Logger.I("task", $"({name}) Started");
 
-
             isCompleted = false;
             isRunning = true;
             isPrepared = true;
@@ -83,6 +168,9 @@ namespace LandmarksR.Scripts.Experiment.Tasks
             StartTimer();
         }
 
+        /// <summary>
+        /// Finishes the task, performing any necessary cleanup.
+        /// </summary>
         protected virtual void Finish()
         {
             if (!isPrepared)
@@ -95,11 +183,18 @@ namespace LandmarksR.Scripts.Experiment.Tasks
             isPrepared = false;
         }
 
+        /// <summary>
+        /// Resets the task to its initial state.
+        /// </summary>
         public virtual void Reset()
         {
             isCompleted = false;
         }
 
+        /// <summary>
+        /// Executes the task and all its subtasks.
+        /// </summary>
+        /// <returns>IEnumerator for coroutine execution.</returns>
         public virtual IEnumerator ExecuteAll()
         {
             if (!_enable) yield break;
@@ -125,17 +220,27 @@ namespace LandmarksR.Scripts.Experiment.Tasks
             Finish();
         }
 
-        //method to join all the subtasks name
+        /// <summary>
+        /// Gets the names of all subtasks.
+        /// </summary>
+        /// <returns>A string containing the names of all subtasks.</returns>
         private string GetSubTasksName()
         {
             return _subTasks.Aggregate("", (current, task) => current + (task.name + " "));
         }
 
+        /// <summary>
+        /// Starts the task timer.
+        /// </summary>
         protected void StartTimer()
         {
             StartCoroutine(TimerCoroutine());
         }
 
+        /// <summary>
+        /// Coroutine to handle the task timer.
+        /// </summary>
+        /// <returns>IEnumerator for coroutine execution.</returns>
         private IEnumerator TimerCoroutine()
         {
             while (elapsedTime < timer && isRunning)
